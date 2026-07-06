4085a1ad35fa5f75bcef7694ba9070e3c42752f6
vinothvkr <vinothvkr@hotmail.com>
2026-07-06 19:56:56 +0530

    feat: integrate Google Analytics and update consts for improved site tracking

--- src/consts.ts
+++ src/consts.ts
@@ -1,27 +1,32 @@
-import type { SvgComponent } from "astro/types"
-import Email from "@/assets/icons/email.svg"
-import GitHub from "@/assets/icons/github.svg"
-import RSS from "@/assets/icons/rss.svg"
-import Twitter from "@/assets/icons/twitter.svg"
+import type { SvgComponent } from 'astro/types'
+import Email from '@/assets/icons/email.svg'
+import GitHub from '@/assets/icons/github.svg'
+import RSS from '@/assets/icons/rss.svg'
+import Twitter from '@/assets/icons/twitter.svg'
+
+export const ANALYTICS = {
+  google: import.meta.env.PUBLIC_GOOGLE_ANALYTICS_ID || '',
+}
 
 export const SITE = {
-  title: "Vinoth Kumar",
-  description: "Website of Vinoth Kumar Rajendran - Software Engineer and Tech Enthusiast.",
-  locale: "en-US",
-  dir: "ltr",
-  defaultPageImage: "/static/opengraph-image.png",
-  defaultPostImage: "/static/1200x630.png",
+  title: 'Vinoth Kumar',
+  description:
+    'Website of Vinoth Kumar Rajendran - Software Engineer and Tech Enthusiast.',
+  locale: 'en-US',
+  dir: 'ltr',
+  defaultPageImage: '/static/opengraph-image.png',
+  defaultPostImage: '/static/1200x630.png',
 } as const
 
 export const NAVIGATION = [
-  { href: "/blog", label: "Blog" },
+  { href: '/blog', label: 'Blog' },
   // { href: "/projects", label: "Projects" },
-  { href: "/about", label: "About" },
+  { href: '/about', label: 'About' },
 ]
 
 export const SOCIALS: { href: string; label: string; icon: SvgComponent }[] = [
-  { href: "https://github.com/vinothvkr", label: "GitHub", icon: GitHub },
-  { href: "https://twitter.com/_vinothvkr", label: "Twitter", icon: Twitter },
-  { href: "mailto:vinothvkr@hotmail.com", label: "Email", icon: Email },
-  { href: "/rss.xml", label: "RSS", icon: RSS },
+  { href: 'https://github.com/vinothvkr', label: 'GitHub', icon: GitHub },
+  { href: 'https://twitter.com/_vinothvkr', label: 'Twitter', icon: Twitter },
+  { href: 'mailto:vinothvkr@hotmail.com', label: 'Email', icon: Email },
+  { href: '/rss.xml', label: 'RSS', icon: RSS },
 ]
